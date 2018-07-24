00dd8cdb2a3c435d98cef7769488b8031c239ceb
William Dong <william.dong@nab.com.au>
2018-07-24 10:31:00 +1000

    spring boot skeleton

---
+++ src/main/java/nwdong/springbootcli/SpringbootExampleCliApplication.java
@@ -0,0 +1,12 @@
+package nwdong.springbootcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExampleCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExampleCliApplication.class, args);
+	}
+}
